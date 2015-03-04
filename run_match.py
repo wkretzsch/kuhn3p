da68991ce7ffae107298fffcfe813d5b4aae8d81
Warren W. Kretzschmar <wkretzsch@gmail.com>
2015-03-04 18:48:37 +0000

    finally fixed the logging issues. I think...

--- run_match.py
+++ run_match.py
@@ -1,6 +1,10 @@
 from kuhn3p import betting, deck, dealer, players
 from random import Random
 
+# to index
+# actor to player order
+def apo(actor, first): return ( actor + first ) % 3
+
 rng         = Random()
 rng.seed(31337)  # each seed corresponds to a different set of hands
 
@@ -21,12 +25,14 @@ log.write("## Definition of player names\n" +
 
 log.write("#hand\tfirst_player_ID\tterminal_state_num\tterminal_state_string" +
 		  "\tdelta_0\tdelta_1\tdelta_2" +
-		  "\twinning_player_ID\tcard_0\tcard_1\tcard_2\tcard_hidden" +
+		  "\twinning_player_ID\tcard_0\tcard_1\tcard_2\tcard_burned" +
+		  "\twallet_0\twallet_1\twallet_2" +
 		  "\n");
 for hand in range(num_hands):
 	first          = hand % 3
 	second         = (first + 1) % 3
 	third          = (second + 1) % 3
+	actor_order    = [first, second, third]
 
 	this_players   = [the_players[first], the_players[second], the_players[third]]
 
@@ -35,12 +41,16 @@ for hand in range(num_hands):
 	for i in range(3):
 		total[(first + i)%3] += delta[i]
 
-        # writing results of hand to log
-        log.write("\t".join([str(hand), str(first), str(state), str(betting.to_string(state)),
-                             "\t".join(map(str, delta)), str(delta.index(max(delta))),
-                            "\t".join( map(str, map(deck.card_to_string, cards)))
-                         ]))
-        log.write("\n")
+	# writing results of hand to log
+	log.write("\t".join([str(hand), str(first), str(state), str(betting.to_string(state)),
+						 "\t".join(map(str, [ delta[apo(i, first)] for i in actor_order ])),
+						 str(apo(dealer.winner(state, cards), first)),
+						 "\t".join( map(str, map(deck.card_to_string,
+												 [ cards[i] for i in [apo(first,first), apo(second,first), apo(third,first), 3] ]
+										 ))),
+						 "\t".join(map(str, total))
+				 ]))
+	log.write("\n")
         
 
 for i in range(3):
