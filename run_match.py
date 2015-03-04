70cb39f832884a35fa53f657b0b40ea16cf91739
Warren W. Kretzschmar <wkretzsch@gmail.com>
2015-03-04 16:54:34 +0000

    added more logging info

--- run_match.py
+++ run_match.py
@@ -12,7 +12,17 @@ the_players = [players.Chump(0.99, 0.01, 0.0),
 total = [0, 0, 0]
 
 log = open('round.log','w');
-log.write("#hand\tfirst_player\tterminal_state_num\tterminal_state_string\n");
+log.write("## Definition of player names\n" +
+          "ID\t0\t" + the_players[0].__class__.__name__ + "\n"
+          "ID\t1\t" + the_players[1].__class__.__name__ + "\n"
+          "ID\t2\t" + the_players[2].__class__.__name__ + "\n"
+		  "## For delta and card numbers starting player is 0 (i.e. delta_0 and card_0)" + "\n"
+)
+
+log.write("#hand\tfirst_player_ID\tterminal_state_num\tterminal_state_string" +
+		  "\tdelta_0\tdelta_1\tdelta_2" +
+		  "\twinning_player_ID\tcard_0\tcard_1\tcard_2\tcard_hidden" +
+		  "\n");
 for hand in range(num_hands):
 	first          = hand % 3
 	second         = (first + 1) % 3
@@ -20,12 +30,16 @@ for hand in range(num_hands):
 
 	this_players   = [the_players[first], the_players[second], the_players[third]]
 
-	(state, delta) = dealer.play_hand(this_players, deck.shuffled(rng))
+	cards = deck.shuffled(rng)
+	(state, delta) = dealer.play_hand(this_players, cards)
 	for i in range(3):
 		total[(first + i)%3] += delta[i]
 
         # writing results of hand to log
-        log.write("\t".join([str(hand), str(first), str(state), str(betting.to_string(state))]))
+        log.write("\t".join([str(hand), str(first), str(state), str(betting.to_string(state)),
+                             "\t".join(map(str, delta)), str(delta.index(max(delta))),
+                            "\t".join( map(str, map(deck.card_to_string, cards)))
+                         ]))
         log.write("\n")
         
 
